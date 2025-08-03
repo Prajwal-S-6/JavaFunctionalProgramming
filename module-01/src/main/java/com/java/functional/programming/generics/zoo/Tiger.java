be6fb70268f12019ebf94ff76217d9520661f499
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-08-03 17:36:23 +0530

    Functional Programming & Generics

---
+++ module-01/src/main/java/com/java/functional/programming/generics/zoo/Tiger.java
@@ -0,0 +1,16 @@
+package com.java.functional.programming.generics.zoo;
+
+public class Tiger extends Animal implements Eats, Runs{
+
+
+
+    @Override
+    public void eats() {
+
+    }
+
+    @Override
+    public void runs() {
+
+    }
+}
