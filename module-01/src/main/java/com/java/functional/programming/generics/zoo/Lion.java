44bf1d38c684acfeebf360439419793e2d288578
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-08-03 17:36:08 +0530

    Functional Programming & Generics

---
+++ module-01/src/main/java/com/java/functional/programming/generics/zoo/Lion.java
@@ -0,0 +1,16 @@
+package com.java.functional.programming.generics.zoo;
+
+public class Lion extends Animal implements Eats, Runs{
+
+
+
+    @Override
+    public void eats() {
+
+    }
+
+    @Override
+    public void runs() {
+
+    }
+}
