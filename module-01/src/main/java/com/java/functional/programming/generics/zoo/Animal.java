8ae7c1915efa3b88b6be69f1b1bda67f84185082
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-08-03 08:45:47 +0530

    Functional Programming & Generics

---
+++ module-01/src/main/java/com/java/functional/programming/generics/zoo/Animal.java
@@ -0,0 +1,16 @@
+package com.java.functional.programming.generics.zoo;
+
+public class Animal {
+
+    private String type;
+
+
+
+    public String getType() {
+        return "--";
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
